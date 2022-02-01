b9b1d8df40a9d5d299fcf26c8e7be7485f85fbf2
Darek Zieliński <zielak@users.noreply.github.com>
2022-02-01 20:01:21 +0100

    upgrades

--- src/helpers.js
+++ src/helpers.js
@@ -1,54 +1,58 @@
-require('array.prototype.flatmap/auto')
-const omit = require('@babel/runtime/helpers/objectWithoutPropertiesLoose')
-const id = x => x
-const render = value => {
-	if(value === null) return null
+const omit = require("@babel/runtime/helpers/objectWithoutPropertiesLoose");
+const id = (x) => x;
+const render = (value) => {
+	if (value === null) return null;
 
-	const jsType = typeof value
-	if(jsType === 'undefined') return null
-	if(jsType === 'boolean') return null
-	if(jsType === 'string') return createElement('text', {value})
-	if(Array.isArray(value)) return value
-	if(typeof value.type === 'function'){
-		return render(value.type({...value, type: null}))
+	const jsType = typeof value;
+	if (jsType === "undefined") return null;
+	if (jsType === "boolean") return null;
+	if (jsType === "string") return createElement("text", { value });
+	if (Array.isArray(value)) return value;
+	if (typeof value.type === "function") {
+		return render(value.type({ ...value, type: null }));
 	}
-	if(typeof value.type === 'string') return value
-	return null
-}
+	if (typeof value.type === "string") return value;
+	return null;
+};
 
-const toArray = input => Array.isArray(input) ? input : [input]
-const flatRender = value => toArray(render(value))
-const toChildrenArray = input => {
-	const parsed = toArray(input).flatMap(flatRender).filter(id)
-	return parsed.length ? parsed : undefined
-}
+const toArray = (input) => (Array.isArray(input) ? input : [input]);
+const flatRender = (value) => toArray(render(value));
+const toChildrenArray = (input) => {
+	const parsed = toArray(input).flatMap(flatRender).filter(id);
+	return parsed.length ? parsed : undefined;
+};
 
-const toClassNameArray = input => {
-	if(Array.isArray(input)){
-		const parsed = input.filter(x => x && typeof x === 'string')
-		return parsed.length ? parsed : undefined
+const toClassNameArray = (input) => {
+	if (Array.isArray(input)) {
+		const parsed = input.filter((x) => x && typeof x === "string");
+		return parsed.length ? parsed : undefined;
 	}
-	if(typeof input === 'string') return toClassNameArray(input.split(/\s+/))
-	return undefined
-}
+	if (typeof input === "string") return toClassNameArray(input.split(/\s+/));
+	return undefined;
+};
 
-const _ = object => JSON.parse(JSON.stringify(render(object)))
+const _ = (object) => JSON.parse(JSON.stringify(render(object)));
 
-const createElement = (type, props, ...args) => _({
-	...omit(props, ['className', 'tagName']), type,
-	children: toChildrenArray(args),
-	data: {
-		hName: props?.tagName,
-		hProperties: toClassNameArray(props?.className) && {
-			className: toClassNameArray(props?.className),
-	}},
-})
+const createElement = (type, props, ...args) =>
+	_({
+		...omit(props, ["className", "tagName"]),
+		type,
+		children: toChildrenArray(args),
+		data: {
+			hName: props?.tagName,
+			hProperties: toClassNameArray(props?.className) && {
+				className: toClassNameArray(props?.className),
+			},
+		},
+	});
 
-const replace = source => ({with: target => {
-	for(let property in source){
-		if(!target.hasOwnProperty(property)) delete source[property]
-	}
-	Object.assign(source, target)
-}})
+const replace = (source) => ({
+	with: (target) => {
+		for (let property in source) {
+			if (!target.hasOwnProperty(property)) delete source[property];
+		}
+		Object.assign(source, target);
+	},
+});
 
-module.exports = {createElement, replace}
+module.exports = { createElement, replace };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 /** @jsx createElement */
 
-const visit = require('unist-util-visit')
-const {createElement, replace} = require('./helpers')
+const visit = require("unist-util-visit");
+const { createElement, replace } = require("./helpers");
 
-module.exports = ({markdownAST}, options = {}) => {
-	visit(markdownAST, 'image', node => {
-		if(!node.alt) return;
+module.exports = ({ markdownAST }, options = {}) => {
+	visit(markdownAST, "image", (node) => {
+		if (!node.alt) return;
 
 		replace(node).with(
-			<unknown tagName='figure' className={options.figureClassName}>
-				<image {...node} alt={null} className={options.imageClassName}/>
-				<paragraph tagName='figcaption' className={options.captionClassName}>
+			<unknown tagName="figure" className={options.figureClassName}>
+				<image {...node} alt={null} className={options.imageClassName} />
+				<paragraph tagName="figcaption" className={options.captionClassName}>
 					{node.alt}
 				</paragraph>
 			</unknown>
-		)
-	})
-}
+		);
+	});
+};
